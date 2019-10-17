cf0b9d50cb759ffe2f689b3904cbfede5449f9b9
최용권 <chldydrnjs94@gmail.com>
2019-10-18 01:07:56 +0900

    project merge

--- src/main/java/com/bu/bumoim/controller/GroupController.java
+++ src/main/java/com/bu/bumoim/controller/GroupController.java
@@ -1,27 +1,31 @@
 package com.bu.bumoim.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bu.bumoim.domain.GroupList;
-import com.bu.bumoim.domain.Member;
 import com.bu.bumoim.service.GroupService;
 
 @Controller
 public class GroupController {
-
-	
+	//
 	private Logger logger = Logger.getLogger(getClass());
+	
 	@Autowired
 	private GroupService groupservice;
 	
@@ -67,4 +71,10 @@ public class GroupController {
 		}
 	}
 
+	// 그룹 => 그룹정보
+	@RequestMapping(value = "/group/groupInfo.do")
+	public String groupInfoView() {
+		//
+		return "group/groupInfo";
+	}
 }
